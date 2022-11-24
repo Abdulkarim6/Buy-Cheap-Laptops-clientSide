b807a69466d58fb336058b5e7abec7f24f06e504
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-11-25 00:17:33 +0600

    added footer section

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -7,13 +7,20 @@ const Product = ({ product }) => {
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image}  className='h-56' alt="Shoes" /></figure>
+            <figure><img src={image} className='h-56' alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">
-                    Shoes!
+                <h2 className="card-title text-3xl">
+                    {name}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='text-semibold'>
+                    <p>Sell Price : {recelPrice}</p>
+                    <p>Original price : {originalPrice}</p>
+                    <p>Seller name : {sellerName}</p>
+                    <p>Post : {postDate}</p>
+                    <p>Location : {location}</p>
+                    <p>used Time : {usedTime}</p>
+                </div>
                 <div className="card-actions justify-end">
                     <div className="badge badge-outline">Fashion</div>
                     <div className="badge badge-outline">Products</div>
